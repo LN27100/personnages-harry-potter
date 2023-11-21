74330dbc4a3013ca9974366fa4c9e82ba18e5f9e
LN27100 <heloceinlove@hotmail.fr>
2023-11-21 15:11:39 +0100

    ajout image liens vers maisons

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 import './App.css'
 import logoPersonnages from '../public/images/personnages.jpg'
 import logoSortileges from '../public/images/sortileges.jpg'
+import chapeau from '../public/images/chapeau.jpg'
 
 function App() {
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={logoPersonnages} className="logo" alt="Vite logo" />
+        <h1 className="texte-ombre">L'univers d'Harry Potter</h1>
+
+        <a href="https://vitejs.dev" target="_blank" className="logo-container">
+          <img src={logoPersonnages} className="logo" alt="logo personnages" />
+          <p className="logo-text">Quel personnage serais-tu?</p>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={logoSortileges} className="logo react" alt="React logo" />
+
+
+        <a href="https://react.dev" target="_blank" className="logo-container">
+          <img src={logoSortileges} className="logo" alt="logo sortilèges" />
+          <p className="logo-text">Trouves ton sortilèges de prédilections.</p>
         </a>
       </div>
-      <h1>L'univers d'Harry Potter</h1>
+
+
       <div className="card">
-       <h2>Découvrez les personnages ou les sortilèges en cliquant sur les images correspondantes !</h2>
-        
+      <a href="https://react.dev" target="_blank" className="logo-container">
+        <img src={chapeau} className="logo" alt="logo maisons" />
+          <p className="logo-text">Quelle sera ta maion !!</p>
+        </a>
+
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
