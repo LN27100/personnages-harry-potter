370affd6ac438257bc5769859ef35bdafe4d8582
LN27100 <heloceinlove@hotmail.fr>
2023-11-21 14:23:01 +0100

    ajout images t background

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logoPersonnages from '../public/images/personnages.jpg'
+import logoSortileges from '../public/images/sortileges.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={logoPersonnages} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={logoSortileges} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>L'univers d'Harry Potter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       <h2>Découvrez les personnages ou les sortilèges en cliquant sur les images correspondantes !</h2>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
