6565980e391753dcb2b52534e819f532760a270d
LN27100 <heloceinlove@hotmail.fr>
2023-11-22 16:37:44 +0100

    affichage de cards avec images et données dans les pages de récup Json

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReactDOM from 'react-dom'; // Importez ReactDOM
 import './App.css';
 import logoPersonnages from '../public/images/personnages.jpg';
 import logoSortileges from '../public/images/sortileges.jpg';
 import chapeau from '../public/images/chapeau.jpg';
 
+// Composant représentant une carte générique
+const Card = ({ data, type }) => {
+  if (type === 'characters') {
+    return (
+      <div className="card">
+        <img src={data.image} alt={data.name} />
+        <h2>Nom: {data.name}</h2>
+        <h3>Acteur: {data.actor}</h3>
+        <p>Maison: {data.house}</p>
+        <p> Espèces: {data.species}</p>
+        <p>Date d'anniversaire: {data.dateOfBirth}</p>
+        <p>Couleur des yeux: {data.eyeColour}</p>
+        <p>Couleur de cheveux: {data.hairColour}</p>
+        <p>Genre: {data.gender}</p>
+        <p>Patronus: {data.patronus}</p>
+        <p>Ascendance : {data.ancestry}</p>
+      </div>
+    );
+  } else if (type === 'spells') {
+    return (
+      <div className="card">
+        <h2>Nom: {data.name}</h2>
+        <p>Nom: {data.description}</p>
+
+      </div>
+    );
+  } else if (type === 'houses') {
+    return (
+      <div className="card">
+<img src={data.image} alt={data.name} />
+        <h2>Nom: {data.name}</h2>
+        <h3>Acteur: {data.actor}</h3>
+        <p>Maison: {data.house}</p>
+        <p> Espèces: {data.species}</p>
+        <p>Date d'anniversaire: {data.dateOfBirth}</p>
+        <p>Couleur des yeux: {data.eyeColour}</p>
+        <p>Couleur de cheveux: {data.hairColour}</p>
+        <p>Genre: {data.gender}</p>
+        <p>Patronus: {data.patronus}</p>
+        <p>Ascendance : {data.ancestry}</p>      </div>
+    );
+  } else {
+    return null; // Type non pris en charge
+  }
+};
+
 // Composant réutilisable représentant un logo cliquable
-const LogoComponent = ({ image, text, apiUrl }) => {
+const LogoComponent = ({ image, text, apiUrl, dataType }) => {
   const navigate = useNavigate();
+  const [data, setData] = useState(null);
 
-  const handleLogoClick = async () => {
-    try {
-      // Effectue une requête vers l'API pour obtenir les données
-      const response = await fetch(apiUrl);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
 
-      // Vérifie si la réponse est réussie (statut 2xx)
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
+        }
+
+        const fetchedData = await response.json();
+        setData(fetchedData);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error.message);
       }
+    };
 
-      // Convertit la réponse en JSON
-      const data = await response.json();
+    fetchData();
+  }, [apiUrl]);
 
-      // Affiche les données récupérées dans la console
-      console.log('Données récupérées avec succès :', data);
-
-      // Ouvre une nouvelle fenêtre avec les données
+  const handleLogoClick = () => {
+    if (data) {
       const newWindow = window.open('', '_blank');
-      newWindow.document.write(JSON.stringify(data));
+      
+      const cards = data.map((item) => (
+        <Card key={item.id} data={item} type={dataType} />
+      ));
 
-      // Redirige l'utilisateur vers une nouvelle page avec les données
-      navigate('/liste-donnees', { state: { data } });
-    } catch (error) {
-      // Gère les erreurs, affiche un message d'erreur dans la console
-      console.error('Erreur lors de la récupération des données :', error.message);
-      // Gérez l'erreur de manière appropriée (affichage d'un message d'erreur à l'utilisateur, etc.)
+      ReactDOM.render(<div>{cards}</div>, newWindow.document.body);
     }
   };
 
@@ -58,6 +108,7 @@ function App() {
           image={logoPersonnages}
           text="Quel personnage serais-tu?"
           apiUrl="https://hp-api.onrender.com/api/characters"
+          dataType="characters"
         />
 
         {/* Deuxième logo cliquable */}
@@ -65,6 +116,7 @@ function App() {
           image={logoSortileges}
           text="Trouves ton sortilège de prédilection."
           apiUrl="https://hp-api.onrender.com/api/spells"
+          dataType="spells"
         />
       </div>
 
@@ -74,6 +126,7 @@ function App() {
           image={chapeau}
           text="Quelle sera ta maison !!"
           apiUrl="https://hp-api.onrender.com/api/characters/house/gryffindor"
+          dataType="houses"
         />
       </div>
     </div>
